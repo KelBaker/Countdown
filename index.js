3fa6a8fe7b8c8b13c95fad476b03cae337b73ab0
Kelvin Henrique <kelbaker56@gmail.com>
2026-08-18 17:51:05 -0300

    fix: corrige lógica da contagem regressiva para usar os data-attributes atuais do HTML

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 function countdown() {
-    var dataFinal = new Date('2024-10-01T00:00:00')
+    var dataFinal = new Date('2026-12-01T00:00:00')
     var dataAtual = new Date()
     var diferenca = dataFinal - dataAtual
+
+    if (diferenca < 0) diferenca = 0
 
     var dias = Math.floor(diferenca / (1000 * 60 * 60 * 24))
     var horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     var minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60))
     var segundos = Math.floor((diferenca % (1000 * 60)) / 1000)
 
-    document.getElementById('Countdown').innerText = dias + 'd ' + horas + 'h ' + minutos + 'm ' + segundos + 's'
-    document.getElementById('Countdown').style.color = 'rgb(199, 33, 11)'
+    var pad = function (n) { return String(n).padStart(2, '0') }
+
+    document.querySelector('[data-dias]').innerText = pad(dias)
+    document.querySelector('[data-horas]').innerText = pad(horas)
+    document.querySelector('[data-minutos]').innerText = pad(minutos)
+    document.querySelector('[data-segundos]').innerText = pad(segundos)
 }
 
-
 setInterval(countdown, 1000)
-countdown() 
+countdown()

3ecb1a2add7b7cbec6178b5507f63005eeca6ebb
Kelvin Henrique <kelbaker56@gmail.com>
2026-08-19 07:53:38 -0300

    fix: corrige ano do evento e permite contagem negativa após o CCXP22

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function countdown() {
-    var dataFinal = new Date('2026-12-01T00:00:00')
+    var dataFinal = new Date('2022-12-01T00:00:00')
     var dataAtual = new Date()
     var diferenca = dataFinal - dataAtual
 
-    if (diferenca < 0) diferenca = 0
+    var negativo = diferenca < 0
+    var abs = Math.abs(diferenca)
 
-    var dias = Math.floor(diferenca / (1000 * 60 * 60 * 24))
-    var horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    var minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60))
-    var segundos = Math.floor((diferenca % (1000 * 60)) / 1000)
+    var dias = Math.floor(abs / (1000 * 60 * 60 * 24))
+    var horas = Math.floor((abs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    var minutos = Math.floor((abs % (1000 * 60 * 60)) / (1000 * 60))
+    var segundos = Math.floor((abs % (1000 * 60)) / 1000)
 
     var pad = function (n) { return String(n).padStart(2, '0') }
 
@@ -16,6 +17,10 @@ function countdown() {
     document.querySelector('[data-horas]').innerText = pad(horas)
     document.querySelector('[data-minutos]').innerText = pad(minutos)
     document.querySelector('[data-segundos]').innerText = pad(segundos)
+
+    document.querySelector('.countdown-label').innerText = negativo
+        ? 'Já se passaram desde a maior CCXP de todos os tempos'
+        : 'Contagem regressiva para a maior CCXP de todos os tempos'
 }
 
 setInterval(countdown, 1000)
